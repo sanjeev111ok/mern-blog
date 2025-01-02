6d9ed060acea60f174822edfc0dee4e968b49a74
Sanjeev Shrestha <sanjeev1.785401@cct.tu.edu.np>
2025-01-02 22:27:59 +0545

    connecting the server with database mongodb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config()
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Mongo db is connected")
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
 const app = express()
 
 app.listen(3000, () => {
