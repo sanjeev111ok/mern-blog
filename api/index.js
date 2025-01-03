6f88a0ef18022daaf87518ba951adcf923229a6a
Sanjeev Shrestha <sanjeev1.785401@cct.tu.edu.np>
2025-01-03 16:54:18 +0545

    add middleware and function to handle error

--- api/index.js
+++ api/index.js
@@ -22,3 +22,12 @@ app.listen(5005, () => {
 })
 app.use("/api/user", userRoutes)
 app.use("/api/auth", authRoutes)
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500
+  const message = err.message || "Internal error"
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  })
+})
