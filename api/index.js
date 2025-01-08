69ff10919a0a84f55c43c5901e3eb09a862c7612
Sanjeev Shrestha <sanjeev1.785401@cct.tu.edu.np>
2025-01-08 14:49:12 +0545

    update user api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 import userRoutes from "./routes/user.route.js"
+import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.route.js"
 
 dotenv.config()
@@ -16,6 +17,7 @@ mongoose
 
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
 
 app.listen(5005, () => {
   console.log("Server is running in port 3000!!")
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,8 @@
 import express from "express"
-import { test } from "../controllers/user.controller.js"
+import { test, updateUser } from "../controllers/user.controller.js"
+import { verfiyToken } from "../utils/verifyUser.js"
 const router = express.Router()
 
 router.get("/test", test)
+router.put("/update/:userId",verfiyToken, updateUser)
 export default router
