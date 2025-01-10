8894b8ed3fe39c6f7333640a2cc1f6ef3dc812e1
Sanjeev Shrestha <sanjeev1.785401@cct.tu.edu.np>
2025-01-10 22:23:45 +0545

    add admin functionality to the user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -45,6 +45,7 @@ export const signin = async (req, res, next) => {
     const token = jwt.sign(
       {
         id: validUser._id,
+        isAdmin: validUser.isAdmin,
       },
       process.env.JWT_SECRET
     )
@@ -65,6 +66,7 @@ export const google = async (req, res, next) => {
       const token = jwt.sign(
         {
           id: user._id,
+          isAdmin: user.isAdmin,
         },
         process.env.JWT_SECRET
       )
@@ -86,7 +88,8 @@ export const google = async (req, res, next) => {
       await newUser.save()
       const token = jwt.sign(
         {
-          id: user._id,
+          id: newUser._id,
+          isAdmin: newUser.isAdmin,
         },
         process.env.JWT_SECRET
       )
