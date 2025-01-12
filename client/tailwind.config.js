d48716d4cfba1b59f2f26cec42dd78ba644bbcb4
Sanjeev Shrestha <sanjeev1.785401@cct.tu.edu.np>
2025-01-12 23:14:38 +0545

    show user post inside dashboard

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,11 @@
-const flowbite = require("flowbite-react/tailwind")
+import flowbite from "flowbite-react/tailwind"
+import scrollbar from "tailwind-scrollbar"
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
     extend: {},
   },
-  plugins: [flowbite.plugin()],
+  plugins: [flowbite.plugin(), scrollbar],
 }
