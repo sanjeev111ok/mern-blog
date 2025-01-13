e37892fa2e36f0914e57c68910aba80677353fc7
Sanjeev Shrestha <sanjeev1.785401@cct.tu.edu.np>
2025-01-13 21:32:06 +0545

    create get users route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -4,6 +4,7 @@ import {
   updateUser,
   deleteUser,
   signout,
+  getusers,
 } from "../controllers/user.controller.js"
 import { verfiyToken } from "../utils/verifyUser.js"
 const router = express.Router()
@@ -13,4 +14,5 @@ router.put("/update/:userId", verfiyToken, updateUser)
 router.delete("/delete/:userId", verfiyToken, deleteUser)
 
 router.post("/signout", signout)
+router.get("/getusers", verfiyToken, getusers)
 export default router
