56b11b5389e33ad696d2a67e1c8346affe821adb
Sanjeev Shrestha <sanjeev1.785401@cct.tu.edu.np>
2025-01-13 22:17:49 +0545

    show users to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,7 +1,12 @@
 import { Sidebar } from "flowbite-react"
 import { Link, useLocation } from "react-router-dom"
 import { useEffect, useState } from "react"
-import { HiArrowSmRight, HiDocumentText, HiUser } from "react-icons/hi"
+import {
+  HiArrowSmRight,
+  HiDocumentText,
+  HiOutlineUserGroup,
+  HiUser,
+} from "react-icons/hi"
 import { useDispatch } from "react-redux"
 import { signoutSuccess } from "../redux/user/userSlice"
 import { useSelector } from "react-redux"
@@ -57,6 +62,17 @@ export default function DashSidebar() {
                 as="div"
               >
                 Posts
+              </Sidebar.Item>
+            </Link>
+          )}
+          {currentUser.isAdmin && (
+            <Link to="/dashboard?tab=users">
+              <Sidebar.Item
+                active={tab === "users"}
+                icon={HiOutlineUserGroup}
+                as="div"
+              >
+                Users
               </Sidebar.Item>
             </Link>
           )}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom"
 import DashSidebar from "../components/DashSidebar.jsx"
 import DashProfile from "../components/DashProfile.jsx"
 import DashPosts from "../components/DashPosts.jsx"
+import DashUsers from "../components/DashUsers.jsx"
 
 export default function Dashboard() {
   const location = useLocation()
@@ -25,6 +26,8 @@ export default function Dashboard() {
       {tab === "profile" && <DashProfile />}
       {/* posts */}
       {tab === "posts" && <DashPosts />}
+      {/* users */}
+      {tab === "users" && <DashUsers />}
     </div>
   )
 }
