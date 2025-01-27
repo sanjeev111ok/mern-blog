4c284191f4b7083f53ca03cf96ac18fabe212c43
Sanjeev Shrestha <sanjeev1.785401@cct.tu.edu.np>
2025-01-27 23:25:39 +0545

    complete about and project section

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>About</div>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold my-7 text-center">
+            About BaaGH Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              This is the blog website that I created to share my thoughts and
+              ideas to the people who are interested to learn programming. I am
+              Backend developer and love to write on programming. Hope you will
+              enjoy my blogs..
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+              iusto vel eaque eveniet molestiae molestias dolor sapiente, quam
+              excepturi modi aliquid officia similique eius recusandae a qui
+              ullam porro, quo consectetur sed. Fuga eius voluptas
+              exercitationem omnis ratione repellat quod laborum molestiae
+              accusantium, quae sint harum maiores ex corrupti voluptatum!
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+              animi nemo vitae dolores earum tenetur perferendis eveniet dolor?
+              Praesentium consequuntur aut dolorum ipsa dicta debitis nostrum
+              odio iste corrupti architecto obcaecati quo consequatur qui
+              eligendi molestiae aperiam saepe, dolorem mollitia tenetur?
+              Tenetur facere doloribus veritatis atque laudantium laborum labore
+              eius natus nisi id ducimus, quasi dolorem eos magni, deleniti aut
+              mollitia. Nisi saepe consequatur fugit consequuntur, voluptate
+              amet accusamus quasi earum sapiente dolor, porro voluptas.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
