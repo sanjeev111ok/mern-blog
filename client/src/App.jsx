c85de4c07b7442ee9ab534afb7338b32de8d1dd7
Sanjeev Shrestha <sanjeev1.785401@cct.tu.edu.np>
2025-01-27 23:07:24 +0545

    add search functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import CreatePost from "./pages/CreatePost.jsx"
 import UpdatePost from "./pages/UpdatePost.jsx"
 import PostPage from "./pages/PostPage.jsx"
 import ScrollToTop from "./components/ScrollToTop.jsx"
+import Search from "./pages/Search.jsx"
 export default function App() {
   return (
     <BrowserRouter>
@@ -23,6 +24,7 @@ export default function App() {
         <Route path="/about" element={<About />}></Route>
         <Route path="/sign-up" element={<SignUp />}></Route>
         <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/search" element={<Search />}></Route>
         <Route element={<PrivateRoute />}>
           <Route path="/dashboard" element={<Dashboard />}></Route>
         </Route>
